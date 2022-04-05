2f580ac9a842ff088f3e302feea508904f23c290
haiacoustic25 <haiacoustic25@gmai.com>
2022-04-05 10:59:07 +0700

    add bootraps

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./HomePage";
-import AdminPage from "./AdminPage";
+import HomePage from "./Features/Client/HomePage";
+// import AdminPage from "./AdminPage";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/admin" element={<AdminPage />} />
+          {/* <Route path="/admin" element={<AdminPage />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/Features/Client/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+
+const HomePage = () => {
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="name@example.com" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Example textarea</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+    </Form>
+  );
+};
+
+export default HomePage;
