3a2898501af1909f3218938b3c0255317d9f854e
haiacoustic25 <haiacoustic25@gmai.com>
2022-04-06 08:08:22 +0700

    add scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./Features/Client/HomePage";
+import HomePage from "./Features/Web/HomePage";
 // import AdminPage from "./AdminPage";
 function App() {
   return (
