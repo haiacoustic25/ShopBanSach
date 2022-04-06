c856428597ccae35130829e276670b646a2da35c
haiacoustic25 <haiacoustic25@gmai.com>
2022-04-06 21:46:37 +0700

    code header

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import "./Assets/boxicons-2.0.7/css/boxicons.min.css";
+import "./Assets/css/grid.css";
+import "./Assets/css/index.css";
 import "./Assets/SCSS/app.scss";
 
 import HomePage from "./Features/Web/Home/HomePage";
-// import AdminPage from "./AdminPage";
-
 import AdminPage from "./Features/Admin/AdminPage";
 
 function App() {
--- client/src/Features/Web/Home/HomePage.jsx
+++ client/src/Features/Web/Home/HomePage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../../../Components/Header/Header";
-
 const HomePage = () => {
   return (
     <>
