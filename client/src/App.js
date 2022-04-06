693f6aa69ea0da49c9685cea0b00de4a80f92ff8
Hieu Dam <hieuvipvt1@gmail.com>
2022-04-06 08:05:22 +0700

    add lib

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Features/Client/HomePage";
-// import AdminPage from "./AdminPage";
+import AdminPage from "./Features/Admin/AdminPage";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          {/* <Route path="/admin" element={<AdminPage />} /> */}
+          <Route path="/admin" element={<AdminPage />} />
         </Routes>
       </BrowserRouter>
     </div>
