52e07eceacc84c0240c616f275e051b02a2e42ae
Hieu Dam <hieuvipvt1@gmail.com>
2022-04-06 16:03:22 +0700

    Step up admin

--- client/src/Features/Admin/AdminPage.jsx
+++ client/src/Features/Admin/AdminPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import '../../Assets/boxicons-2.0.7/css/boxicons.min.css'
+import '../../Assets/css/grid.css'
+import '../../Assets/css/index.css'
+import layout from "./Components/layout/layout";
+
 const AdminPage = () => {
-  return <div>AdminPage</div>;
+  return (
+    <layout />
+  );
 };
 
 export default AdminPage;
