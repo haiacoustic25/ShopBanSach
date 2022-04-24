1b6740ce56907af2295115910eca99753f289a01
Hieu Dam <hieuvipvt1@gmail.com>
2022-04-24 23:24:08 +0700

    edit admin

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,6 @@ import ScrollTop from "./Assets/Img/ScrollTop.png";
 import HomePage from "./Features/Web/Home/HomePage";
 import ProductDetail from "./Features/Web/ProductDetail/ProductDetail";
 import CartPage from "./Features/Web/Cart/CartPage";
-
-import Users from "./Features/Admin/Pages/Users/Users";
-import Single from "./Features/Admin/Pages/Single/Single";
-import New from "./Features/Admin/Pages/new/New";
-import { productInputs, userInputs } from "./Database/formSource";
-import Product from "./Features/Admin/Pages/Products/Product";
-import Login from "./Features/Web/User/Login";
-import Register from "./Features/Web/User/Register";
 import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import { theme } from "./Features/Admin/theme";
 import { Layout } from "./Features/Admin/Pages/Layout";
@@ -40,56 +32,22 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route path="/admin">
-              <Route index element={<AdminPage />} />
-              <Route path="users">
-                <Route index element={<Users />} />
-                <Route path=":userId" element={<Single />} />
-                <Route
-                  path="new"
-                  element={<New inputs={userInputs} title="Add New User" />}
-                />
-              </Route>
-              <Route path="products">
-                <Route index element={<Product />} />
-                <Route path=":productId" element={<Single />} />
-                <Route
-                  path="new"
-                  element={
-                    <New inputs={productInputs} title="Add New Product" />
-                  }
-                />
-              </Route>
-            </Route>
-            <Route index element={<HomePage />} />
-
-            <Route path="/:id" element={<ProductDetail />} />
-            <Route path="/gio-hang" element={<CartPage />} />
-            <Route path="/dang-nhap" element={<Login />} />
-            <Route path="/dang-ky" element={<Register />} />
-
-            {/* <Route path="/:id" element={<ProductDetail />} /> */}
-          </Route>
-        </Routes>
-
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Routes>
-            <Route path="/">
-              <Route index element={<HomePage />} />
-              <Route path="/:id" element={<ProductDetail />} />
-              <Route path="/cart" element={<CartPage />} />
-              <Route path="/admin" element={<Layout />}>
-                <Route index element={<Dashboard />} />
-                <Route path="Users" element={<Users />} />
-                <Route path="Products" element={<Products />} />
-                <Route path="Orders" element={<Orders />} />
-              </Route>
-            </Route>
-          </Routes>
-        </ThemeProvider>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <Routes>
+                <Route path="/">
+                  <Route index element={<HomePage />} />
+                  <Route path="/:id" element={<ProductDetail />} />
+                  <Route path="/cart" element={<CartPage />} />
+                  <Route path="/admin" element={<Layout />}>
+                    <Route index element={<Dashboard />} />
+                    <Route path="Users" element={<Users />} />
+                    <Route path="Products" element={<Products />} />
+                    <Route path="Orders" element={<Orders />} />
+                  </Route>
+                </Route>
+              </Routes>
+            </ThemeProvider>
       </BrowserRouter>
       {/* scroll to top */}
       {scrollPosition >= 100 && (
