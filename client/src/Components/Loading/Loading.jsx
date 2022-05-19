4e4dba5d6c7025c54cef5db2139cdd2653de07f9
haiacoustic25 <haiacoustic25@gmai.com>
2022-05-19 11:21:47 +0700

    decentralization and fix peoductDetail

--- client/src/Components/Loading/Loading.jsx
+++ client/src/Components/Loading/Loading.jsx
@@ -3,10 +3,10 @@ import "../../Assets/SCSS/loading.scss";
 const Loading = () => {
   return (
     <div className="loadingPage">
-      <div class="spinner">
-        <span class="loading spinner-inner-1"></span>
-        <span class="loading spinner-inner-2"></span>
-        <span class="loading spinner-inner-3"></span>
+      <div className="spinner">
+        <span className="loading spinner-inner-1"></span>
+        <span className="loading spinner-inner-2"></span>
+        <span className="loading spinner-inner-3"></span>
       </div>
     </div>
   );
